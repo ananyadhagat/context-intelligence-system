5b73b5bb6db9e5b28e75479b4bafd2627f2dc2ff
ananyadhagat <146732397+ananyadhagat@users.noreply.github.com>
2026-07-06 19:58:03 +0530

    Initial Spring Boot project setup

---
+++ context-intelligence-system/src/main/java/context_intelligence_system/ContextIntelligenceSystemApplication.java
@@ -0,0 +1,13 @@
+package context_intelligence_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextIntelligenceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContextIntelligenceSystemApplication.class, args);
+	}
+
+}
---
+++ context-intelligence-system/src/test/java/context_intelligence_system/ContextIntelligenceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package context_intelligence_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContextIntelligenceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
